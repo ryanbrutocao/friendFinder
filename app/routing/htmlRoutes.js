3196e578ec87153c56391974e408f35f04c1cee3
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-09-10 18:31:02 -0600

    work in progress

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -18,6 +18,9 @@ app.get("/", function(req, res) {
 app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "/../public/survey.html"));
 });
+// app.get('*',function (req, res) {
+//   res.sendFile(path.join(__dirname, "/../public/home.html"));
+// });
 
 
 
