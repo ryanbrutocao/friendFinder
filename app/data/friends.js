95e68b2b9417410501c26a8525d53a9aa15a9f1e
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-09-12 22:14:02 -0600

    completed homework

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friends = [
   {
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
     "scores":[
         5,
         1,
         4,
-        // 4,
-        // 5,
-        // 1,
-        // 2,
-        // 5,
-        // 4,
-        // 1
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
       ]
   }
   
