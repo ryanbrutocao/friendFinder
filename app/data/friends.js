29ffa56d4150f4c0bfa6e366dbc0a949c9350388
Ryan Brutocao <ryanbrutocao@outlook.com>
2019-12-11 14:32:33 -0700

    updating for Demo day.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,71 @@
 var friends = [
   {
-    "name":"Ahmed",
-    "photo":"https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "name": "Adam",
+    "photo": "https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "scores": [
+      5,
+      2,
+      4,
+      3,
+      5,
+      3,
+      2,
+      2,
+      4,
+      1
+    ]
   }
-  
-  
+  // , {
+  //   "name": "Ben",
+  //   "photo": "https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  //   "scores": [
+  //     1,
+  //     1,
+  //     5,
+  //     4,
+  //     3,
+  //     5,
+  //     2,
+  //     2,
+  //     2,
+  //     4
+  //   ]
+  // }, {
+  //   "name": "Cheryl",
+  //   "photo": "https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  //   "scores": [
+  //     3,
+  //     2,
+  //     4,
+  //     4,
+  //     5,
+  //     4,
+  //     2,
+  //     5,
+  //     2,
+  //     3
+  //   ]
+  // }, {
+  //   "name": "Dian",
+  //   "photo": "https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  //   "scores": [
+  //     2,
+  //     1,
+  //     3,
+  //     4,
+  //     4,
+  //     1,
+  //     5,
+  //     5,
+  //     4,
+  //     2
+  //   ]
+  // }
+
+
+
+
+
 ];
 console.log("----------friends below-----");
 console.log(friends);
